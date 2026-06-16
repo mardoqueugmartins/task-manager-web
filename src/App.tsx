934d627b491aae81bbb697dd0a082c7ae25e4bf6
Mardoqueu Martins <dev.mardoqueumartins@gmail.com>
2026-06-16 17:45:18 -0300

    refactor: extract task from component

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,55 @@
-import { useEffect, useState } from 'react';
-import { api } from './services/api';
-import type { Task } from './types/Task';
-import TaskList from './components/TaskList';
-
+import { useEffect, useState } from "react";
+import { api } from "./services/api";
+import type { Task } from "./types/Task";
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
 
 function App() {
-
   const [tasks, setTasks] = useState<Task[]>([]);
 
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
   useEffect(() => {
     const fetchTask = async () => {
-      const response = await api.get('/tasks');
-        console.log(response.data)
-        setTasks(response.data);
-    }
+      const response = await api.get("/tasks");
+      console.log(response.data);
+      setTasks(response.data);
+    };
 
     fetchTask();
+  }, []);
 
-}, []);
-
-const handleCreateTask = async () => {
-  const response = await api.post(
-    '/tasks',
-    {
+  const handleCreateTask = async () => {
+    const response = await api.post("/tasks", {
       title,
-      description
-    }
-  );
-  setTasks([...tasks, response.data])
+      description,
+    });
+    setTasks([...tasks, response.data]);
 
-  setTitle('');
-  setDescription('');
-  console.log(response.data)
-};
+    setTitle("");
+    setDescription("");
+    console.log(response.data);
+  };
 
-
-console.log(tasks)
+  console.log(tasks);
 
   return (
     <div>
       <h1>Task Manager</h1>
       <p>Total de tarefas: {tasks.length}</p>
-      <label> Titulo:</label>
-      <input type="text" value={title} onChange={(event) => {
-        setTitle(event.target.value);
-      }}/>
-      <label> Descrição:</label>
-      <input type="text" value={description} onChange={(event) => {
-        setDescription(event.target.value);
-      }} />
-      <button onClick={handleCreateTask}>Criar Tarefa</button>
-     <TaskList tasks={tasks} />
+
+      <TaskForm
+        title={title}
+        description={description}
+        setTitle={setTitle}
+        setDescription={setDescription}
+        handleCreateTask={handleCreateTask}
+      />
+
+      <TaskList tasks={tasks} />
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,8 +1,25 @@
+type TaskFormProps = {
+  title: string;
+  description: string;
 
-const TaskForm = () => {
+  setTitle: (value: string) => void;
+  setDescription: (value: string) => void;
+
+  handleCreateTask: () => void;
+};
+
+const TaskForm = ({title, description, setTitle, setDescription, handleCreateTask}: TaskFormProps) => {
   return (
     <div>
-      
+      <label> Titulo:</label>
+      <input type="text" value={title} onChange={(event) => {
+        setTitle(event.target.value);
+      }}/>
+      <label> Descrição:</label>
+      <input type="text" value={description} onChange={(event) => {
+        setDescription(event.target.value);
+      }} />
+      <button onClick={handleCreateTask}>Criar Tarefa</button>
     </div>
   )
 }
